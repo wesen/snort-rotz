bc4dc167a5a81f4f7bfe5f882f09c883d04600b1
manuel <manuel@bf09bf55-38ec-0310-9d3f-cd34fc72d291>
2002-01-28 01:54:17 +0000

    fixed gen_content
    git-svn-id: svn+ssh://bl0rg.net/srv/svn/snort-rotz@667 bf09bf55-38ec-0310-9d3f-cd34fc72d291

--- gen-packet.pl
+++ gen-packet.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl -w
 #
 # Generates and sends packets matching snort rules
-# $Id: gen-packet.pl,v 1.9 2002-01-28 00:43:27 manuel Exp $
+# $Id: gen-packet.pl,v 1.10 2002-01-28 01:54:17 manuel Exp $
+#
+# TODO: uricontent, regex, rpc
 
 use strict;
 use Net::IPv4Addr qw( ipv4_parse );
@@ -23,12 +25,16 @@ my $ethint = "eth0";
 # Are we on the same network, so we can spoof destination addresses too?
 my $localnet = 1;
 
+# Do we want debug?
+#my $debug = "rule,payload,params";
+my $debug = "rule,params";
+
 ####################
 # Helper functions #
 ####################
 
 sub tbl_rand {
-   return $_[rand($#_ + 1)];
+   return $_[int(rand($#_ + 1))];
 }
 
 sub ip_to_int {
@@ -54,8 +60,18 @@ sub int_to_ip {
 sub gen_port {
    my $port = shift;
 
+   if ($port =~ /^:(.*)$/) {
+      $port = $1
+   } else {
+      $port =~ s/:.*$//;
+   }
+   
+   if ($port =~ /!([0-9]*)$/) {
+      $port = $1 + 1;
+   }
+
    if ($port eq "any") {
-      return rand(65535) + 1;
+      return int(rand(65535) + 1);
    } else {
       return $port;
    }
@@ -82,7 +98,7 @@ sub gen_ip {
          # Netmask
          if (defined($cidr) and defined($ip)) {
             my $nip = ip_to_int($ip);
-            $nip = $nip + ((rand (2 ** 32 + 1)) % (2 ** (32 - $cidr)));
+            $nip = $nip + (int(rand (2 ** 32 + 1)) % (2 ** (32 - $cidr)));
             return int_to_ip($nip);
          } elsif (defined($ip)) {
             return $ip;
@@ -100,8 +116,22 @@ sub gen_rand_tbl {
    my ($len) = @_;
    my @pad;
 
-   push @pad, rand(256) for (1 .. $len);
+   push @pad, int(rand(256)) for (1 .. $len);
    return @pad;
+}
+
+sub gen_num {
+   my $str = shift;
+
+   if ($str =~ /^>([0-9]*)/) {
+      return ($1 + 1);
+   } elsif ($str =~ /^<([0-9]*)/) {
+      return ($1 - 1);
+   } elsif ($str =~ /^!([0-9]*)/) {
+      return $str + 1;
+   } else {
+      return $str;
+   }
 }
 
 # Generate a matching packet content
@@ -109,20 +139,19 @@ sub gen_content {
    my ($content) = @_;
    my @data;
 
-   print "gen_content $content\n";
-
    if (defined($content)) {
       $content =~ s/\\"/"/g;
       $content =~ s/\\:/:/g;
 
-      while (length($content) > 0) {
+      while (defined($content) and (length($content) > 0)) {
          if ($content =~ s/^([^\|]+)//) {
+            my $str = $1;
+
+            $str =~ s/[^\\]\\$/|/;
+            $str =~ s/\\(.)/$1/g;
+
             # handle escape characters
-            push(@data, map(ord, split(//, $1)));
-            if ((substr($1, -1, 1)) eq "\\") {
-               push(@data, substr($content, 0, 1));
-               $content = substr($content, 1);
-            }
+            push(@data, map(ord, split(//, $str)));
          }
          if ($content =~ s/^\|(.+?)\|//) {
             my $str = $1;
@@ -130,7 +159,10 @@ sub gen_content {
             $str =~ s/(..)/$1 /g;
             my @values = split(/\s/, $str);
             push(@data, map(hex, @values));
-         }
+         } elsif ($content =~ /^\|$/) {
+            # Handle special case (\|)
+            return @data;
+         } 
       }
    }
 
@@ -165,9 +197,9 @@ sub gen_ipparams {
 
    $ipparams{tos} = $params->{tos} if (defined($params->{tos}));
    if (defined($params->{ttl})) {
-      $ipparams{ttl} = $params->{ttl};
+      $ipparams{ttl} = gen_num($params->{ttl});
    } else {
-      $ipparams{ttl} = rand 63;
+      $ipparams{ttl} = int(rand(63));
    }
    $ipparams{id} = $params->{id} if (defined($params->{id}));
 
@@ -183,13 +215,12 @@ sub gen_tcpparams {
    $tcpparams{dest} = gen_port($dstport);
    
    # seq, ack
-   $tcpparams{seq} = $params->{seq} if (defined($params->{seq}));
-   $tcpparams{ack_seq} = $params->{ack} if (defined($params->{ack}));
+   $tcpparams{seq} = gen_num($params->{seq}) if (defined($params->{seq}));
+   $tcpparams{ack_seq} = gen_num($params->{ack}) if (defined($params->{ack}));
    
    $tcpparams{data} = $payload;
    
    # flags: (F, S, R, P, A, U, 2, 1, 0)
-   # XXX We don't catch "!+*"
    if (defined($params->{flags})) {
       my %bla = (F => "fin",
                  S => "syn",
@@ -227,18 +258,41 @@ sub gen_icmpparams {
    my %icmpparams;
    
    # itype, icode, icmp_id, icmp_seq
-   $icmpparams{sequence} = $params->{icmp_seq} 
+   $icmpparams{sequence} = gen_num($params->{icmp_seq}) 
       if (defined($params->{icmp_seq}));
-   $icmpparams{type} = $params->{itype} 
+   $icmpparams{type} = gen_num($params->{itype}) 
       if (defined($params->{itype}));
-   $icmpparams{code} = $params->{icode} 
+   $icmpparams{code} = gen_num($params->{icode}) 
       if (defined($params->{icode}));
-   $icmpparams{id} = $params->{icmp_id} 
+   $icmpparams{id} = gen_num($params->{icmp_id}) 
       if (defined($params->{icmp_id}));
    
    $icmpparams{data} = $payload;
 
    return \%icmpparams;
+}
+
+###################
+# Debug functions #
+###################
+
+# Debug payload
+sub debug_payload {
+   my ($payload) = @_;
+
+   print "payload real ".join(" ", (map { sprintf "%02x", $_ }
+            unpack("C*", $payload)))."\n";
+   print "payload real ".join(" ", (map { sprintf "%c", $_ }
+            unpack("C*", $payload)))."\n";
+}
+
+# Debug parameter list
+sub debug_params {
+   my ($params) = @_;
+
+   foreach my $key (keys %{$params}) {
+      print "$key\: ".$params->{$key}."\n" if (!($key eq "data"));
+   }
 }
 
 #############
@@ -254,13 +308,7 @@ while (<>) {
       my (%pktparams, %params);
       my (@payload, $payload);
 
-      $dstport =~ s/:.*$//;
-      $srcport =~ s/:.*$//;
-
-      #print "rule: $rule";
-      #print "proto $proto, srcip $srcip, srcport $srcport\n";
-      #print "dstip $dstip, dstport $dstport\n";
-      #print "params: $params\n";
+      print "rule: $rule" if ($debug =~ /rule/);
 
       while ($params =~ /\s?(.*?)\"?\s*\;/g) {
          my $opt = $1;
@@ -270,7 +318,7 @@ while (<>) {
                if (defined($params{depth}) and 
                      ($params{depth} > $#payload)) {
                   for (my $i = $#payload + 1; $i < $params{depth}; $i++) {
-                     push (@payload, rand(256));
+                     push (@payload, int(rand(256)));
                   }
                }
                push(@payload, @tmpdata);
@@ -283,33 +331,33 @@ while (<>) {
       }
       if (defined($params{dsize}) && ($params{dsize} =~ />([0-9]*)/)) {
          my $size = $1 + 1;
-         print "SIZE $size $#payload\n";
          push(@payload, gen_rand_tbl($size - $#payload));
       }
 
       $payload = pack("C*", @payload);
-
-      #print "payload      ".join(" ", (map { sprintf "%02x", $_ }
-      #         @payload))."\n";
-      #print "payload real ".join(" ", (map { sprintf "%02x", $_ }
-      #         unpack("C*", $payload)))."\n";
-      #print "payload real ".join(" ", (map { sprintf "%c", $_ }
-      #         unpack("C*", $payload)))."\n";
       
       # we don't like fragbits
       next if (defined($params{fragbits}) or
                defined($params{ipoption}));
 
+      # we don't like packets that are too big (in fact, Net::RawIP
+      # doesn't)
+      next if ($#payload >= 4000);
+
       $pktparams{ip} = gen_ipparams($srcip, $dstip, \%params);
 
       if ($proto eq "tcp") {
          $pktparams{tcp} = gen_tcpparams($srcport, $dstport, \%params, $payload);
+         debug_params($pktparams{tcp}) if ($debug =~ /params/);
       } elsif ($proto eq "udp") {
          $pktparams{udp} = gen_udpparams($srcport, $dstport, \%params, $payload);
+         debug_params($pktparams{udp}) if ($debug =~ /params/);
       } elsif($proto eq "icmp") {
          $pktparams{icmp} = gen_icmpparams(\%params, $payload);
+         debug_params($pktparams{icmp}) if ($debug =~ /params/);
       }
 
+      debug_payload($payload) if ($debug =~ /payload/);
       my $pkt = new Net::RawIP(\%pktparams);
       $pkt->ethnew($ethint);
       $pkt->ethset(source => 'de:ad:de:ad:be:ef',
@@ -317,7 +365,6 @@ while (<>) {
       $pkt->ethsend;
 
       print $params{msg}."\n";
-      <STDIN>;
-
+      #<STDIN>;
    }
 }
